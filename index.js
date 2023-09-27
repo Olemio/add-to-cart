afa88e2a765c89afbd6ba67059784c160139c11f
Oliver Bing <ailurus842@gmail.com>
2023-09-27 13:25:33 +0200

    add comments

--- index.js
+++ index.js
@@ -14,67 +14,91 @@ const addButtonEl = document.getElementById("add-button")
 const clearButton = document.getElementById("clear-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
-addButtonEl.addEventListener("click", function() {
+addButtonEl.addEventListener("click", function () {
     let inputValue = inputFieldEl.value
-    
+
+    //push input field value to db
     push(shoppingListInDB, inputValue)
-    
+
     clearInputFieldEl()
 })
 
-clearButton.addEventListener("dblclick", function() {
+clearButton.addEventListener("dblclick", function () {
+    // create path to db
     let shoppingListDbLocation = ref(database, `shoppingList`)
-        
+
+    // remove everything from db
     remove(shoppingListDbLocation)
 })
 
-inputFieldEl.addEventListener("keyup", function(e) {
+// listening for "Enter" press
+inputFieldEl.addEventListener("keyup", function (e) {
     if (e.key === "Enter") {
         let inputValue = inputFieldEl.value
-    
+
+        //push input field value to db on "Enter" press
         push(shoppingListInDB, inputValue)
 
         clearInputFieldEl()
     }
 })
 
-onValue(shoppingListInDB, function(snapshot) {
+//
+onValue(shoppingListInDB, function (snapshot) {
     if (snapshot.exists()) {
+        // [#lq34j3øl5hl2 {<-- ID}, Bread]
         let itemsArray = Object.entries(snapshot.val())
-    
+
+        // empty HTML shopping list
         clearShoppingListEl()
-        
+
+        // add new items form db back to HTML shopping list
         for (let i = 0; i < itemsArray.length; i++) {
             let currentItem = itemsArray[i]
-            
+
+            // add one item in HTML shopping list
             appendItemToShoppingListEl(currentItem)
-        }    
+        }
     } else {
+        // nothing in db
         shoppingListEl.innerHTML = "No items here... yet"
     }
 })
 
+// clear HTML shopping list
 function clearShoppingListEl() {
     shoppingListEl.innerHTML = ""
 }
 
+// clear input field text
 function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
 
+
 function appendItemToShoppingListEl(item) {
+
+    // #lq34j3øl5hl2 {<-- ID}
     let itemID = item[0]
+
+    //  Bread
     let itemValue = item[1]
-    
+
+    // newEl = <li></li>
     let newEl = document.createElement("li")
-    
+
+    // newEl = <li>Bread</li>
     newEl.textContent = itemValue
-    
-    newEl.addEventListener("dblclick", function() {
+
+    // newEl = <li ondblclick={ funksjonenUnder() }>Bread</li>
+    newEl.addEventListener("dblclick", function () {
+        // finner tingen i db basert på id
         let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
-        
+
+        // fjerner tingen
         remove(exactLocationOfItemInDB)
     })
-    
+
+    // add <li ondblclick={funksjonenOver() }>Bread</li> til HTML shopping liste
     shoppingListEl.append(newEl)
 }
